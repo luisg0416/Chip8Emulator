21e8eab854c35867bc4a0a4e21f4e0a2c0dc8c1b
luisg0416 <lhguerrero0416@gmail.com>
2026-06-08 20:13:45 -0400

    First commit

---
+++ Display.cs
@@ -0,0 +1,8 @@
+namespace Chip8Emulator
+{
+    public class Display
+    {
+        private bool[,] display = new bool[32, 64];
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+bool running = true;
+Stack <ushort> s = new Stack<ushort>();
+Stopwatch stopwatch = new Stopwatch();
+
+while (running) {
+    stopwatch.Start();
+
+    stopwatch.Stop();
+
+
+    long ts = stopwatch.ElapsedMilliseconds;
+    Thread.Sleep((int)(16L - ts));
+}
