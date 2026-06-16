5ae82701e91462d3c11ca43fc60252bba494b65a
luisg0416 <lhguerrero0416@gmail.com>
2026-06-15 21:59:27 -0400

    Add SDL2 rendering and verify IBM logo displays correctly

--- Program.cs
+++ Program.cs
@@ -1,18 +1,74 @@
 ﻿using System.Diagnostics;
 using System.Threading;
 using Chip8Emulator;
+using static SDL2.SDL;
 
-bool running = true;
-Stopwatch stopwatch = new Stopwatch();
-Memory memory = new Memory("");
+SDL_Init(SDL_INIT_VIDEO);
+
+IntPtr window = SDL_CreateWindow(
+    "CHIP-8 Emulator",
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    640,
+    320,
+    SDL_WindowFlags.SDL_WINDOW_SHOWN
+);
+
+IntPtr renderer = SDL_CreateRenderer(window, -1, SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+
+Memory memory = new Memory("C:\\Users\\Luis\\Chip8Emulator\\IBM Logo.ch8");
 Display display = new Display();
 CPU cpu = new CPU(memory, display);
 
+bool running = true;
+Stopwatch stopwatch = new Stopwatch();
+
 while (running) {
-    stopwatch.Start();
+    stopwatch.Restart();
+
+    while (SDL_PollEvent(out SDL_Event e) != 0) {
+        if (e.type == SDL_EventType.SDL_QUIT) {
+            running = false;
+        }
+    }
+
+    for (int i = 0; i < 11; i++) {
+        cpu.Cycle();
+    }
+
+    if (cpu.delayTimer > 0) cpu.delayTimer--;
+    if (cpu.soundTimer > 0) cpu.soundTimer--;
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    for (int y = 0; y < 32; y++) {
+        for (int x = 0; x < 64; x++) {
+            if (display.display[y, x]) {
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            } else {
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            }
+
+            SDL_Rect rect = new SDL_Rect {
+                x = x * 10,
+                y = y * 10,
+                w = 10,
+                h = 10
+            };
+
+            SDL_RenderFillRect(renderer, ref rect);
+        }
+    }
+
+    SDL_RenderPresent(renderer);
+
     stopwatch.Stop();
-
-
     long ts = stopwatch.ElapsedMilliseconds;
-    Thread.Sleep((int)(16L - ts));
+    if (16L - ts > 0)
+        Thread.Sleep((int)(16L - ts));
 }
+
+SDL_DestroyRenderer(renderer);
+SDL_DestroyWindow(window);
+SDL_Quit();
--- obj/Debug/net10.0/Chip8Emulator.AssemblyInfo.cs
+++ obj/Debug/net10.0/Chip8Emulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chip8Emulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbfe740eef480d40bc46586b48fe5cd10a89baed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a907eb784999a47def7c05a28dfc2f213fec9ee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chip8Emulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chip8Emulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
